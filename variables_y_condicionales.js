240c6de82c7d1c468f134c315173dcd46caa6d13
GastonVillalba <gastonvill1984@gmail.com>
2023-06-10 12:37:03 -0300

    ejercicios instituto javascript

---
+++ variables_y_condicionales.js
@@ -0,0 +1,79 @@
+// Ejercicio 1 Realiza una aplicación que solicite al usuario su nombre y apellido y lo muestre en pantalla//
+
+/*let nombre_y_apellido=prompt("Coloque su nombre y apellido");
+alert(nombre_y_apellido);
+
+/* Ejercicio 2 Realiza una aplicación que le solicite
+al usuario su nombre y su edad y le muestre por 
+pantalla el mensaje “Hola XX tu edad es XX” donde XX son los datos del usuario.*/
+
+/*let nombre=prompt("Coloque su nombre");
+let edad=prompt("Coloque su edad");
+alert("Hola "+ nombre +"tu edad es "+ edad)*/
+
+/* Ejercicio 3 Realiza una aplicación que dados dos valores colocados en variables realice la suma de los 
+mismos y los muestre por pantalla.*/
+
+/*let num1=parseInt(prompt("Coloque un numero"));
+let num2=parseInt(prompt("Coloque otro numero"));
+let suma=num1+num2
+alert("la suma es "+suma)*/
+
+
+/* Ejercicio 4 Realiza una calculadora con las operaciones básicas (suma, resta multiplicación y división). 
+Los valores para las operaciones deben estar almacenados en variables previamente.*/
+
+/*let num1=3;
+let num2=4;
+
+let suma=num1+num2;
+let resta=num1-num2;
+let multiplicacion=num1*num2;
+let division=num1/num2;
+
+console.log(suma, resta, multiplicacion, division)*/
+
+/* CONDICIONALES*/
+
+/* Ejercicio 5 Realiza una aplicación que solicite una edad al usuario e indique si es mayor a 18 años o 
+menor.*/
+
+/*let edad = parseInt(prompt("Coloque su edad"));
+if (edad >= 18) {
+    alert("es mayor")
+}
+else {
+    (alert("es menor"))
+}*/
+
+/* Ejercicio 6 Realiza una aplicación que dado dos valores numéricos ingresados por el usuario, indique 
+en pantalla cual es el mayor y cual es el menor.*/
+
+let num1=parseInt(prompt("coloque un numero"));
+let num2=parseInt(prompt("coloque otro numero"));
+if(num1>num2){
+    alert("el mayor es "+num1)
+}
+if(num1==num2){
+    alert("los numeros son iguales")
+    
+} 
+else{
+    alert("el mayor es "+num2)
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
